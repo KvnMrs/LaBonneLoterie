49c583e9f6cf924d95c63b995559822459e510cf
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-29 12:48:24 +0200

    creation item-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { CardItemComponent } from './card-item/card-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    CardItemComponent
   ],
   imports: [
     BrowserModule,
