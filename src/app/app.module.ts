e99e5f7610aefdc4cbb9e846cc5e37950cfa6677
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-29 13:14:36 +0200

    add nav for list-items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { CardItemComponent } from './card-item/card-item.component';
+import { ListItemsComponent } from './list-items/list-items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    CardItemComponent
+    CardItemComponent,
+    ListItemsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: ListItemsComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
