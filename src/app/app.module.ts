a31d38d980bae72c9a68177156c00b96f52feacd
Kévin Marais <kevin.mrs2020@gmail.com>
2022-09-30 10:07:25 +0200

    add the new component and ngForm to the modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,7 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { CardItemComponent } from './card-item/card-item.component';
 import { ListItemsComponent } from './list-items/list-items.component';
 import { HomeComponent } from './home/home.component';
+import { AddItemComponent } from './add-item/add-item.component';
 
 @NgModule({
   declarations: [
@@ -16,16 +19,19 @@ import { HomeComponent } from './home/home.component';
     CardItemComponent,
     ListItemsComponent,
     HomeComponent,
+    AddItemComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'liste', component: ListItemsComponent },
+      { path: 'ajout-annonce', component: AddItemComponent },
     ])
   ],
-  providers: [],
+  providers: [NgForm],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
