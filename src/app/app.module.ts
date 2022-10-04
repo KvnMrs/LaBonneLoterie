91ac5c599d121a2c7593e17e11cdf50f551c2c93
Kévin Marais <kevin.mrs2020@gmail.com>
2022-10-04 20:44:14 +0200

    create component and service upload-img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { NgForm, FormsModule, ReactiveFormsModule  } from '@angular/forms';
-
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment';
 
 // Components
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { CardItemComponent } from './components/card-item/card-item.component';
 import { ListItemsComponent } from './components/list-items/list-items.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { AddItemComponent } from './components/add-item/add-item.component';
+import { UploadImgComponent } from './components/upload-img/upload-img.component';
+import { ItemDetailsComponent } from './components/item-details/item-details.component';
 
 // Services
 import { AnnouncesService } from './services/announces/announces.service'
 import { AuthService } from './services/auth/auth.service';
 import { AuthGuardService } from './services/auth/auth-guard.service';
-import { ItemDetailsComponent } from './components/item-details/item-details.component';
-// import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-// import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { UploadImgService } from './services/uploads/upload-img.service';
 
 const appRoutes : Routes = [
   { path: 'auth', component: AuthComponent },
@@ -40,17 +40,25 @@ const appRoutes : Routes = [
     AuthComponent,
     AddItemComponent,
     ItemDetailsComponent,
+    UploadImgComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
   ],
-  providers: [NgForm, AnnouncesService, AuthService, AuthGuardService],
+  providers: [
+    NgForm,
+    AnnouncesService,
+    AuthService,
+    AuthGuardService,
+    UploadImgService,
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
