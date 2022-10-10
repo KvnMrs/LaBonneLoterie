2f26f834923995ce3e182bc24a3b80623bdbb619
Kévin Marais <kevin.mrs2020@gmail.com>
2022-10-10 09:43:55 +0200

    create createAccount servicefunction

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,17 +1,51 @@
 import { Injectable } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  isAuth = false
-  constructor() { }
+  isAuth = false;
+  constructor(private router: Router, private auth: Auth) {}
 
-  signIn(){
-    return new Promise((resolve, rejects) => {setTimeout(() => {this.isAuth = true; resolve(true) }, 200)})
+  form = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  createAccount(email: string, password: string) {
+    createUserWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log('user --->', user);
+        // ...
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorMessage);
+        // ..
+      });
   }
 
-  signOut(){
-    return this.isAuth = false
+  signOut() {
+    return (this.isAuth = false);
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.isAuth) return true;
+    else return this.router.navigate(['auth']);
   }
 }
