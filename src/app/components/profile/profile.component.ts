44976b29e13ebede6fa2ff270b219f9f4eddf180
Kévin Marais <kevin.mrs2020@gmail.com>
2022-10-12 11:14:59 +0200

    create infos profile template

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  public stars = [1, 2, 3, 4, 5];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
