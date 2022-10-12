19f216d8f1e2e42ad60792638aeb8cfeec95f2ee
Kévin Marais <kevin.mrs2020@gmail.com>
2022-10-12 13:40:59 +0200

    create routing for log-in page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,14 +19,19 @@ import { AddItemComponent } from './components/add-item/add-item.component';
 import { ItemDetailsComponent } from './components/item-details/item-details.component';
 import { BuyTickectComponent } from './components/buy-tickect/buy-tickect.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { SignInComponent } from './components/auth/sign-in/sign-in.component';
+
 // Services
 import { AnnouncesService } from './services/announces/announces.service';
 import { AuthService } from './services/auth/auth.service';
 import { UploadImgService } from './services/uploads/upload-img.service';
-import { SignInComponent } from './components/auth/sign-in/sign-in.component';
 
 const appRoutes: Routes = [
   { path: '', component: SignUpComponent },
+  {
+    path: 'auth',
+    component: SignInComponent,
+  },
   {
     path: 'liste',
     /* canActivate: [AuthGuardService], */ component: ListItemsComponent,
