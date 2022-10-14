161d76aea9b89c7a32a2e39a23d4fd2232379dbc
Kévin Marais <kevin.mrs2020@gmail.com>
2022-10-14 13:40:12 +0200

    create IUserProfile

--- src/app/models/user/user.model.ts
+++ src/app/models/user/user.model.ts
@@ -5,3 +5,15 @@ export interface IUser {
   email: string;
   password: string;
 }
+
+export interface IUserProfile {
+  uid: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  announcesUser: Array<string>;
+  imgUrl: string;
+  phoneNumber: string;
+  rate: number;
+}
