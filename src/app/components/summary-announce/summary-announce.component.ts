be63efcd588d108ea0d68b34c30f433e57d27652
Kévin Marais <kevin.mrs2020@gmail.com>
2023-04-25 00:48:13 +0200

    Feature: Add modal 'wish success' after to publish a new announce.

--- src/app/components/summary-announce/summary-announce.component.ts
+++ src/app/components/summary-announce/summary-announce.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-summary-announce',
   templateUrl: './summary-announce.component.html',
-  styleUrls: ['./summary-announce.component.scss']
+  styleUrls: ['./summary-announce.component.scss'],
 })
 export class SummaryAnnounceComponent implements OnInit {
+  constructor(public router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public redirectUser() {
+    this.router.navigate(['']);
   }
-
 }
