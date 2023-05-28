11248c831288b7d7ce5ba81d6e97b9c441e29231
Kévin Marais <kevin.mrs2020@gmail.com>
2023-05-28 10:11:17 +0200

    Improve: Using dynamic on Profile page.

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DocumentData } from 'firebase/firestore';
+import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
@@ -9,12 +10,16 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 })
 export class UserProfileComponent implements OnInit {
   public stars = [1, 2, 3, 4, 5];
-  public profileData: DocumentData | undefined;
+  public profileData!: DocumentData;
   modalUpdateProfile = false;
+
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.profileData = this.authService.userData;
+    this.profileData['memberSince'] = new Date(
+      this.profileData['memberSince']['seconds']
+    );
   }
 
   updateProfile() {
