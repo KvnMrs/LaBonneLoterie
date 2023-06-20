15abd2b88863e405b01ad09d14f71b9a91503bd8
Kévin Marais <kevin.mrs2020@gmail.com>
2023-06-20 08:42:37 +0200

    ok

--- src/app/components/modals/modals-profile/credit/modal-credit-profile.component.ts
+++ src/app/components/modals/modals-profile/credit/modal-credit-profile.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { IUser } from 'src/app/models/user/user.model';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { UserService } from 'src/app/services/users/user.service';
 
@@ -8,9 +10,10 @@ import { UserService } from 'src/app/services/users/user.service';
   templateUrl: './modal-credit-profile.component.html',
   styleUrls: ['./modal-credit-profile.component.scss'],
 })
-export class ModalCreditProfileComponent implements OnInit {
+export class ModalCreditProfileComponent implements OnInit, OnDestroy {
+  public currentUserSubscription!: Subscription;
   public creditForm!: FormGroup;
-  private currentUser!: string;
+  public currentUser!: IUser;
   @Input() profileData: any;
   loading: boolean = false;
 
@@ -20,18 +23,31 @@ export class ModalCreditProfileComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.currentUser = this.authService.currentUser;
+    // this.authService.currentUserSubject;
     this.creditForm = new FormGroup({
       bankAccount: new FormControl(0, Validators.required),
+    });
+    this.authService.currentUserSubject.subscribe({
+      next: (user) => {
+        this.currentUser = user as IUser;
+        console.log('this.currentUser --->', this.currentUser);
+      },
+      error: (error) => {
+        console.error('Erreur récupération utilisateur.', error);
+      },
     });
   }
 
   onCreditAcoount() {
-    console.log(this.currentUser);
-    console.log(this.creditForm.value.bankAccount);
-    this.userService.creditUserAccount(
-      this.currentUser,
-      this.creditForm.value.bankAccount
-    );
+    // console.log(this.currentUser);
+    // console.log(this.creditForm.value.bankAccount);
+    // this.userService.creditUserAccount(
+    //   this.currentUser,
+    //   this.creditForm.value.bankAccount
+    // );
+  }
+
+  ngOnDestroy(): void {
+    this.currentUserSubscription.unsubscribe();
   }
 }
