c09bc323c4502aa7cd69764d9d905ab52e00b06d
Kévin Marais <kevin.mrs2020@gmail.com>
2023-07-30 03:46:38 +0200

    feat: implement timer front on mobile

--- src/app/components/announce/timer/timer/timer.component.ts
+++ src/app/components/announce/timer/timer/timer.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.scss']
+  styleUrls: ['./timer.component.scss'],
 })
 export class TimerComponent implements OnInit {
+  @Input() hideText? = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
