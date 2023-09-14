9025fa625b2fc5d2fda220f6d1bb850d31a7df88
Kévin Marais <kevin.mrs2020@gmail.com>
2023-09-15 00:13:03 +0200

    Feat: implement cross add svg.

---
+++ src/app/shared/libs/ux-ui/SVGs/cross-add/svg-cross-add.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-cross-add',
+  templateUrl: './svg-cross-add.component.html',
+  styleUrls: ['./svg-cross-add.component.scss'],
+})
+export class SvgCrossAddComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
