7497848613e166fc1d556ef9e8e7ea426e2e8b6b
Kévin Marais <kevin.mrs2020@gmail.com>
2023-09-27 13:58:57 +0200

    ng g c lbl-alert-error.

---
+++ src/app/shared/libs/ux-ui/alert/error/lbl-error.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'lbl-alert-error',
+  templateUrl: './lbl-error.component.html',
+  styleUrls: ['./lbl-error.component.scss'],
+})
+export class LblErrorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
