465ae9a58ba6a2fca8f6d524989d75412059f65d
KvnMrs <kevin.mrs2020@gmail.com>
2024-02-21 01:04:56 +0100

    feature: add a message for the case that there no announce.

--- src/app/shared/libs/ux-ui/svg/eye-closed/eye-closed.component.ts
+++ src/app/shared/libs/ux-ui/svg/eye-closed/eye-closed.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-eye-closed',
-  templateUrl: './eye-closed.component.html',
+  templateUrl: './eye-closed.component.svg',
   styleUrls: ['./eye-closed.component.scss']
 })
 export class EyeClosedComponent implements OnInit {
