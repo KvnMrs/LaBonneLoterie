16a1e60cf5ff0f40f2033582a061674f7910c79b
KvnMrs <kevin.mrs2020@gmail.com>
2024-02-21 18:10:37 +0100

    Add a message for favorites.

--- src/app/shared/libs/ux-ui/svg/heart-cross/heart-cross.component.ts
+++ src/app/shared/libs/ux-ui/svg/heart-cross/heart-cross.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-heart-cross',
-  templateUrl: './heart-cross.component.html',
+  templateUrl: './heart-cross.component.svg',
   styleUrls: ['./heart-cross.component.scss']
 })
 export class HeartCrossComponent implements OnInit {
