94f91064cb10e3cd376b28bc3297e188256048cd
KvnMrs <kevin.mrs2020@gmail.com>
2024-03-26 20:32:50 +0100

    feat: create cloud function checkAnnounceEndAt. (WIP)

--- functions/index.js
+++ functions/index.js
@@ -5,45 +5,25 @@
 // See a full list of supported triggers at https://firebase.google.com/docs/functions
 const {logger} = require("firebase-functions");
 const {onRequest} = require("firebase-functions/v2/https");
-const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const { onDocumentUpdated } = require("firebase-functions/v2/firestore");
 
 // The Firebase Admin SDK to access Firestore.
-const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
 initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addmessage = onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Firestore using the Firebase Admin SDK.
-    const writeResult = await getFirestore()
-        .collection("messages")
-        .add({original: original});
-    // Send back a message that we've successfully written the message
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
-  });
+exports.checkAnnounceEndAt = onDocumentUpdated(
+  "/Announces/{documentId}",
+  (event) => {
+    if (event.data.after.data()["winner"]) return null;
+    const ticketsBuyed = event.data.after.data()["ticketsBuyed"];
+    const randomIndex = Math.floor(Math.random() * ticketsBuyed.length);
+    const winnerTicket = ticketsBuyed[randomIndex];
 
-
-// Listens for new messages added to /messages/:documentId/original
-// and saves an uppercased version of the message
-// to /messages/:documentId/uppercase
-exports.makeuppercase = onDocumentCreated("/messages/{documentId}", (event) => {
-    // Grab the current value of what was written to Firestore.
-    const original = event.data.data().original;
-
-    console.log('original --->', original)
-  
-    // Access the parameter `{documentId}` with `event.params`
-    logger.log("Uppercasing", event.params.documentId, original);
-  
-    const uppercase = original.toUpperCase();
-  
-    // You must return a Promise when performing
-    // asynchronous tasks inside a function
-    // such as writing to Firestore.
-    // Setting an 'uppercase' field in Firestore document returns a Promise.
-    return event.data.ref.set({uppercase}, {merge: true});
-  });
+    return event.data.after.ref.update(
+      { winner: winnerTicket },
+      { merge: true }
+    );
+  }
+);
